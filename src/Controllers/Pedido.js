cef151982b2f262b3e086bda34830bf646e62db9
afelipinas <ganesdevelop@gmail.com>
2023-03-01 14:28:18 -0300

    finalizando pedido

--- src/Controllers/Pedido.js
+++ src/Controllers/Pedido.js
@@ -13,12 +13,11 @@ export async function selectPedidos(req, res){
 
 //para selecionar apenas um pedido (Get)
 export async function selectPedido(req, res){
-    let cod_pedido =req.body.cod_pedido
-    openDb().then(db=>{
-      db.get('SELECT * FROM pedido WHERE cod_pedido=?', [cod_pedido])
-        .then(pedido=>res.json(pedido))
-     });
- }
+    openDb()
+    .then(db => {
+        db.all('SELECT San.nome, San.preco, Cli.nome_cliente FROM Sanduiche San INNER JOIN Pedido Ped ON Ped.cod_sanduiche = San.cod_sanduiche INNER JOIN Cliente Cli ON Cli.cod_cliente = Ped.cod_cliente').then(pessoas => res.json(pessoas))
+    });
+}
 
  //para inserir novos pedidos (post)
 export async function insertPedido(req, res){
@@ -35,9 +34,33 @@ export async function insertPedido(req, res){
 export async function updatePedido(req, res){
     let pedido = req.body
     openDb().then(db=>{
-        db.run('UPDATE pedido SET cod_sanduiche=?, cod_bebida=?, cod_cliente=? email=?, email=?, email=? WHERE cod_cliente=? ', [cliente.nome, cliente.email, cliente.senha, cliente.cod_cliente]);
+        db.run('UPDATE pedido SET cod_sanduiche=?, cod_bebida=?, cod_cliente=?, valor_total=?, status_pedido=?, data_pedido=? WHERE cod_pedido=? ', [pedido.cod_sanduiche, pedido.cod_bebida, pedido.cod_cliente,pedido.valor_total, pedido.status_pedido, pedido.data_pedido, pedido.cod_pedido]);
     });
     res.json({
         "statusCode": 200
     })
 }
+
+//para atualizar dados do pedido (patch)
+export async function patchPedido(req, res){
+    let pedido = req.body;
+    openDb()
+    .then(db => {
+        db.run('UPDATE pedido SET cod_sanduiche=?, cod_bebida=?, cod_cliente=?, valor_total=?, status_pedido=?, data_pedido=? WHERE cod_pedido=? ', [pedido.cod_sanduiche, pedido.cod_bebida, pedido.cod_cliente,pedido.valor_total, pedido.status_pedido, pedido.data_pedido, pedido.cod_pedido]);
+    });
+    res.json({
+        "statusCode": 200
+    })
+}
+
+//para excluir o pedido (delete)
+export async function deletePedido(req, res){
+    let cod_pedido = req.body.cod_pedido
+    openDb().then(db=>{
+        db.get('DELETE FROM pedido WHERE cod_pedido=?', [cod_pedido])
+        .then(res=>res)
+     });
+     res.json({
+        "statusCode": 200
+    })
+ } 
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { insertPessoa, updatePessoa, selectPessoa, selectPessoas, deletePessoa, 
 import { getSanduiches, getSanduiche, postSanduiche, putSanduiche, patchSanduiche, deleteSanduiche} from './Controllers/Sanduiche.js';
 import { deleteCliente, insertCliente, selectCliente, selectClientes, updateCliente, patchCliente} from './Controllers/Cliente.js';
 import './Controllers/Sanduiche.js'
+//import { deletePedido, insertPedido, patchPedido, selectPedido, selectPedidos, updatePedido } from './Controllers/Pedido.js';
 
 
 const router = Router();
@@ -35,8 +36,14 @@ router.put('/cliente', updateCliente); //rota para atualizar dados do cliente
 router.patch('/cliente', patchCliente); //rota para atualizar dados do cliente
 router.delete('/cliente', deleteCliente); //rota para excluir dados do cliente
 
-
-
+/*
+router.get('/pedidos', selectPedidos); //rota para selecinar dados de vários pedido
+router.get('/pedido', selectPedido); //rota para selecionar dados de um pedido
+router.post('/pedido', insertPedido); //rota para adicionar dados de novo pedido
+router.put('/pedido', updatePedido); //rota para atualizar dados do pedido
+router.patch('/pedido', patchPedido); //rota para atualizar dados do pedido
+router.delete('/pedido', deletePedido); //rota para excluir dados do pedido
+*/
 
 router.post('/login', loginPessoa)
 
